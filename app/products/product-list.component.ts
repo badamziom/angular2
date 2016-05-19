d2f36b57189ebe033d29d803b17017d1875261e3
badamziom <badamziom@o2.pl>
2016-05-19 15:02:45 +0200

    Add more components

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -1,18 +1,24 @@
 
 
-import {Component} from "angular2/core";
+import {Component, OnInit} from "angular2/core";
+import {IProduct} from "./product";
+import {ProductFilterPipe} from "./product-filter.pipe";
+import {StarComponent} from "../shared/star.component";
 @Component({
     selector: 'pm-products',
-    templateUrl: 'app/products/product-list.component.html'
+    templateUrl: 'app/products/product-list.component.html',
+    styleUrls: ['app/products/product-list.component.css'],
+    pipes: [ProductFilterPipe],
+    directives: [StarComponent]
 })
 
-export class ProductListComponent{
+export class ProductListComponent implements OnInit{
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string = 'cart';
-    products: any[] = [
+    products: IProduct[] = [
         {
             "productId": 1,
             "productName": "Leaf Rake",
@@ -67,5 +73,13 @@ export class ProductListComponent{
 
     toggleImage(): void {
         this.showImage = !this.showImage;
+    }
+
+    ngOnInit(){
+        console.log('In OnInit');
+    }
+
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Product List: ' + message;
     }
 }
