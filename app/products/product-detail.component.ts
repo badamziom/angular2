e28be2cf0db472953b6806551e5e0be3098a659c
Adam <aszulist@o2.pl>
2016-05-21 07:25:34 +0200

    add product detail, fixed view, finish course

--- app/products/product-detail.component.ts
+++ app/products/product-detail.component.ts
@@ -1,16 +1,34 @@
 import {Component} from "angular2/core";
 import {RouteParams, Router} from "angular2/router";
+import {IProduct} from "./product";
+import {ProductService} from "./product.service";
+import {StarComponent} from "../shared/star.component";
 
 @Component({
-    templateUrl: 'app/products/product-detail.component.html'
+    templateUrl: 'app/products/product-detail.component.html',
+    directives: [StarComponent]
 })
 
 export class ProductDetailComponent {
     pageTitle: string = 'Product Detail';
+    product: IProduct;
+    errorMessage: string;
 
-    constructor(private _routeParams: RouteParams, private _router: Router){
+    constructor(private _productService: ProductService, private _routeParams: RouteParams, private _router: Router){
+    }
+
+    ngOnInit() {
         let id = +this._routeParams.get('id');
-        this.pageTitle += ` : ${id}`;
+        //this.pageTitle += ` : ${id}`;
+        this.getProduct(id);
+    }
+
+    getProduct(id: number) {
+        this._productService.getProduct(id)
+            .subscribe(
+                product => this.product = product,
+                error => this.errorMessage = error
+            );
     }
 
     onBack(): void {
